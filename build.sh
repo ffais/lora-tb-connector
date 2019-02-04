0b9551d80835337b3da6edccb9bf1b0a98240bea
ffais <ffais@fbk.eu>
2019-02-04 18:00:53 +0100

    test

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo $RELEASE
 Msg="$TSSRV Build in corso"
 echo ${ChatID}
 echo ${Token}
-idchat= ${ChatID}
+idchat= $ChatID
 echo $idchat
 curl -s — max-time $TimeLim -d "chat_id=${ChatID}&disable_web_page_preview=1&text=${Msg}" "https://api.telegram.org/bot${Token}/sendMessage" > /dev/null
 echo $Msg
