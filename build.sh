b953d3cadfbec22a1c9731bc9635b8bbd276f0d7
ffais <ffais@fbk.eu>
2019-02-05 11:16:06 +0100

    test

--- build.sh
+++ build.sh
@@ -12,16 +12,16 @@ echo $RELEASE
 Msg="$TSSRV Build in corso"
 curl -s — max-time $TimeLim -d "chat_id=${CHAT_ID}&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
 echo $Msg
-ssh -i sshkey -o "StrictHostKeyChecking no" dev@52.209.70.46 'sudo service lora-tb-conn stop && /home/dev/sources/deploy-lora-tb-conn.sh && echo "VER='${RELEASE}'" > /home/dev/lora-tb-connector-env && sudo service lora-tb-conn start '
-echo $?
-if [ $? -eq 0 ]
-then
-  Msg="$TSSRV Aggiornamento completato"
-  curl -s — max-time $TimeLim -d "chat_id=${CHAT_ID}&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
-  echo $Msg
-else
-  Msg="$TSSRV Aggiornamento non riuscito $?"
-  curl -s — max-time $TimeLim -d "chat_id=${CHAT_ID}&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
-  echo $Msg
-fi
+#ssh -i sshkey -o "StrictHostKeyChecking no" dev@52.209.70.46 'sudo service lora-tb-conn stop && /home/dev/sources/deploy-lora-tb-conn.sh && echo "VER='${RELEASE}'" > /home/dev/lora-tb-connector-env && sudo service lora-tb-conn start '
+#echo $?
+#if [ $? -eq 0 ]
+#then
+#  Msg="$TSSRV Aggiornamento completato"
+#  curl -s — max-time $TimeLim -d "chat_id=${CHAT_ID}&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+#  echo $Msg
+#else
+#  Msg="$TSSRV Aggiornamento non riuscito $?"
+#  curl -s — max-time $TimeLim -d "chat_id=${CHAT_ID}&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+#  echo $Msg
+#fi
 #rm sshkey
