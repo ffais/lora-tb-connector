2ff1a7de7c04aaea08238c05ccf435d5b133ffcf
ffais <ffais@fbk.eu>
2019-02-05 09:48:18 +0100

    test

--- build.sh
+++ build.sh
@@ -23,17 +23,17 @@ echo $RELEASE
 Msg="$TSSRV Build in corso"
 echo $c
 echo $t
-curl -s — max-time $TimeLim -d 'chat_id=$ChatID&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot$Token/sendMessage' > /dev/null
+curl -s — max-time $TimeLim -d 'chat_id=$ChatID&disable_web_page_preview=1&text=$Msg' "https://api.telegram.org/bot$Token/sendMessage" > /dev/null
 echo $Msg
 ssh -i sshkey -o "StrictHostKeyChecking no" dev@52.209.70.46 'sudo service lora-tb-conn stop && /home/dev/sources/deploy-lora-tb-conn.sh && echo "VER='${RELEASE}'" > /home/dev/lora-tb-connector-env && sudo service lora-tb-conn start '
 if [ $? -eq 0 ]
 then
   Msg="$TSSRV Aggiornamento completato"
-  curl -s — max-time $TimeLim -d 'chat_id=$ChatID&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot$Token/sendMessage' > /dev/null
+  curl -s — max-time $TimeLim -d 'chat_id=$ChatID&disable_web_page_preview=1&text=$Msg' "https://api.telegram.org/bot$Token/sendMessage" > /dev/null
   echo $Msg
 else
   Msg="$TSSRV Aggiornamento non riuscito $?"
-  curl -s — max-time $TimeLim -d 'chat_id=$ChatID&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot$Token/sendMessage' > /dev/null
+  curl -s — max-time $TimeLim -d 'chat_id=$ChatID&disable_web_page_preview=1&text=$Msg' "https://api.telegram.org/bot$Token/sendMessage" > /dev/null
   echo $Msg
 fi
 rm sshkey
