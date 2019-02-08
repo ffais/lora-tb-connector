4b861b216f94d7204ca986449861d31f9ee8fadc
ffais <ffais@fbk.eu>
2019-02-08 16:08:09 +0100

    test

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ CHAT="chat_id=${CHAT_ID}"
 curl -s -X POST $URL -d $CHAT -d "text=$Msg"
 #curl -s — max-time $TimeLim -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$IP "sudo service lora-tb-conn stop && /home/$USR/sources/deploy-lora-tb-conn.sh && echo VER=${RELEASE} > /home/dev/lora-tb-connector-env && sudo service lora-tb-conn start "
-echo $?
 if [ $? -eq 0 ]
 then
   Msg="$TSSRV Aggiornamento completato"
