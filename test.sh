a0e1c374a98005fec7baffbcea558051b9aa1271
ffais <ffais@fbk.eu>
2019-02-13 13:43:38 +0100

    test

--- test.sh
+++ test.sh
@@ -29,25 +29,25 @@ then
     echo "test ok"
     Msg="$TSSRV test ok"
     curl -s -X POST $URL -d $CHAT -d "text=$Msg"
-    docker-compose -f lora-tb-connnector-test-yaml down
+    docker-compose -f lora-tb-connnector-test.yaml down
     statusCode=0
   else
     echo "test failed"
-    docker-compose -f lora-tb-connnector-test-yaml down
+    docker-compose -f lora-tb-connnector-test.yaml down
     Msg="$TSSRV test failed"
     curl -s -X POST $URL -d $CHAT -d "text=$Msg"
     statusCode=1
   fi
 else
   echo "starting containter failed"
-  docker-compose -f lora-tb-connnector-test-yaml down
+  docker-compose -f lora-tb-connnector-test.yaml down
   Msg="$TSSRV starting containter failed"
   curl -s -X POST $URL -d $CHAT -d "text=$Msg"
 fi
-docker-compose -f lora-tb-connnector-test-yaml down
+docker-compose -f lora-tb-connnector-test.yaml down
 docker system prune -f
 ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$IP "sudo service lora-tb-conn start"
 rm sshkey
 rm lora-tb-connector.env
 echo $statusCode
-return $statusCode
+exit $statusCode
