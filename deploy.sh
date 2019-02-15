e74213404304865636d52f7903aa2dd75dbe3f5c
ffais <ffais@fbk.eu>
2019-02-15 15:55:23 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,17 @@ URL="https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
 CHAT="chat_id=${CHAT_ID}"
 curl -s -X POST $URL -d $CHAT -d "text=$Msg"
 ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$INTIP "kubectl set image deployments/lora-tb-connector lora-tb-connector=smartcommunitylab/lora-tb-connector:$RELEASE"
-ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$INTIP "kubectl get deployments lora-tb-connector -o json" > dpstatus
-availableReplicas=$(cat dpstatus | jq -r '.status.availableReplicas')
-updatedReplicas=$(cat dpstatus | jq -r '.status.updatedReplicas')
-readyReplicas=$(cat dpstatus | jq -r '.status.readyReplicas' )
-if [[ availableReplicas -eq 1 && updatedReplicas -eq 1 && readyReplicas -eq 1 ]]; then
-  statusCode=0
-  Msg="$TSSRV Deploy ok"
-  curl -s -X POST $URL -d $CHAT -d "text=$Msg"
+ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$INTIP "kubectl rollout status deployment lora-tb-connector"
+if [[ $? -eq 0 ]]; then
+  ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$INTIP "kubectl get deployments lora-tb-connector -o json" > dpstatus
+  availableReplicas=$(cat dpstatus | jq -r '.status.availableReplicas')
+  updatedReplicas=$(cat dpstatus | jq -r '.status.updatedReplicas')
+  readyReplicas=$(cat dpstatus | jq -r '.status.readyReplicas' )
+  if [ $availableReplicas == '1' ] && [ $updatedReplicas == '1' ] && [ $readyReplicas == '1' ]; then
+    statusCode=0
+    Msg="$TSSRV Deploy ok"
+    curl -s -X POST $URL -d $CHAT -d "text=$Msg"
+  fi
 fi
 rm sshkey
 rm dpstatus
