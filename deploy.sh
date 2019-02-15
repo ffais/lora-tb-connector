d313b89943041ab9b975f3210305ea137e5053bf
ffais <ffais@fbk.eu>
2019-02-15 15:13:25 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,21 @@ APP="lora-tb-connector-prod"
 TSTAMP=$(date +%Y.%m.%d-%H.%M.%S)
 TSSRV="$TSTAMP $APP:"
 RELEASE=$(sed -E -n '/<artifactId>(lora-tb-connector)<\/artifactId>.*/{n;p}' pom.xml | grep -Po '\d\.\d')
-Msg="$TSSRV Build in corso"
+Msg="$TSSRV Deploy in corso"
 URL="https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
 CHAT="chat_id=${CHAT_ID}"
 curl -s -X POST $URL -d $CHAT -d "text=$Msg"
-#curl -s — max-time $TimeLim -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$Msg" "https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$INTIP "kubectl set image deployments/lora-tb-connector lora-tb-connector=smartcommunitylab/lora-tb-connector:$RELEASE"
+ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$INTIP "kubectl get deployments lora-tb-connector -o json > dpstatus"
+availableReplicas=$(cat dpstatus | jq -r '.status.availableReplicas')
+updatedReplicas=$(cat dpstatus | jq -r '.status.updatedReplicas')
+readyReplicas=$(cat dpstatus | jq -r '.status.readyReplicas' )
+if [[ availableReplicas -eq 1 && updatedReplicas -eq 1 && readyReplicas -eq 1 ]]; then
+  statusCode=0
+  Msg="$TSSRV Deploy ok"
+  curl -s -X POST $URL -d $CHAT -d "text=$Msg"
+fi
 rm sshkey
+rm dpstatus
 echo $statusCode
 exit $statusCode
